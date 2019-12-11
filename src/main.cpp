c917449b99074f6c4c2224651eefc4bf27ade992
Luke Chu <lukechu10@gmail.com>
2019-12-11 20:35:49 +0000

    feat: print numbers in binary

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,26 @@
 #include "Moitie.h"
 #include <fstream>
 #include <iostream>
+#include <bitset>
 
 using namespace std;
+
+enum class WhoWin : char { Player1, Player2, Tie };
+/*
+bool theoryCorrect(const Moitie &game, WhoWin &who) {
+  // deconstruct into powers of two
+  int n1 = int exp1;
+  for ()
+    int exp2;s
+}
+*/
 
 void writeDataToCSV() {
   ofstream csvData;
   csvData.open("data.csv");
   // repeat for different data
   for (unsigned x = 0; x < 3000; x += 2) {
-    for (unsigned y = 0; y < 3000; y += 2) {
+    for (unsigned y = 0; y <= x; y += 2) {
       Moitie game(x, y);
       auto move = bestMove(game);
       csvData << x << "&" << y << "(" << move.first << "&" << move.second
@@ -21,8 +32,18 @@ void writeDataToCSV() {
   csvData.close();
 }
 
+void findLoose(unsigned num1) {
+  for (unsigned num2 = 1500; num2 < 2500; num2 += 2) {
+    Moitie game(num1, num2);
+    auto move = bestMove(game);
+    if (move.first < 0 && move.second < 0) {
+      cout << num2 << "\n";
+    }
+  }
+}
+
 int main() {
-  /*
+  
   // std::cout << "Hello World!\n";
   int first;
   int second;
@@ -33,9 +54,13 @@ int main() {
   Moitie game(first, second);
   while (!game.IsFinalState()) {
     auto move = bestMove(game);
+
     cout << "T" << game.mTurnsCount << "\tP" << !game.IsPlayerOne() + 1 << ": "
          << game.FirstNumber() << " " << game.SecondNumber() << "\t("
-         << move.first << " " << move.second << ")" << endl;
+         << move.first << " " << move.second << ")   ";
+    
+    cout << "\tBinary: " << bitset<16>(game.FirstNumber()) << " " << bitset<16>(game.SecondNumber()) << endl;
+    
     if (move.first == 0 && move.second == 0) {
       cout << "TIE\n";
       return 0;
@@ -47,8 +72,11 @@ int main() {
   cout << game.FirstNumber() << ", " << game.SecondNumber() << endl;
   cout << "PLAYER " << game.IsPlayerOne() + 1 << " WINS!\n";
   cout << endl;
-*/
+
+  /*
   cout << "Started" << endl;
-  writeDataToCSV();
+  //   writeDataToCSV();
+        //   findLoose(2304);
   cout << "Finished" << endl;
+*/
 }
